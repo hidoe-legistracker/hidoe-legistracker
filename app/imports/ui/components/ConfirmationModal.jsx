aa859a9fedb0fdaf6c805c063895303ea3e04407
Ethan Chee <ethancheez@gmail.com>
2022-09-01 21:34:13 -1000

    Update confirmation modal component

--- app/imports/ui/components/ConfirmationModal.jsx
+++ app/imports/ui/components/ConfirmationModal.jsx
@@ -1,40 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-bootstrap/Modal';
-import { Button } from 'react-bootstrap';
 
-const ConfirmationModal = ({ modal }) => {
-  const [show, setShow] = useState(false);
-  const modalClose = () => setShow(false);
-  const modalShow = () => setShow(true);
-
-  return (
-    <div>
-      <Button variant="success" onClick={modalShow}>
-        Launch demo modal
-      </Button>
-      <Modal show={show} onHide={modalClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title centered>{modal.type} Bill</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <p>Are you sure you want to {modal.type} this bill?</p>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="secondary" onClick={modalClose}>Cancel</Button>
-          <Button variant="primary">Confirm</Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  );
-};
+const ConfirmationModal = ({ modal }) => (
+  <div>
+    <Modal.Header closeButton>
+      <Modal.Title centered>{modal.title}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <p>{modal.body}</p>
+    </Modal.Body>
+  </div>
+);
 
 // Require a document to be passed to this component.
 ConfirmationModal.propTypes = {
   modal: PropTypes.shape({
-    type: PropTypes.string,
+    title: PropTypes.string,
+    body: PropTypes.string,
   }).isRequired,
 };
 
