66124979b17a8c7665fe327a4ee6fcbc356feed0
adamjparrilla <ajp808@hawaii.edu>
2022-09-01 19:24:22 -1000

    brief class work

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-// import { PAGE_IDS } from '../utilities/PageIDs';
+import { PAGE_IDS } from '../utilities/PageIDs';
 
 /** Render a Not Found page if the user enters a URL that doesn't match any route. */
 const Profile = () => (
-  <Container />
+  <Container >
+
+  </Container>
 );
 
 export default Profile;
--- app/imports/ui/utilities/PageIDs.js
+++ app/imports/ui/utilities/PageIDs.js
@@ -8,6 +8,7 @@ export const PAGE_IDS = {
   MANAGE_DATABASE: 'manage-database',
   NOT_AUTHORIZED: 'not-authorized',
   NOT_FOUND: 'not-found',
+  PROFILE: 'profile',
   SIGN_IN: 'sign-in',
   SIGN_OUT: 'sign-out',
   SIGN_UP: 'sign-up',
