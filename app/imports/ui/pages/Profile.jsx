ab027804f05dfeec0305c244d230a8a183d72391
adamjparrilla <ajp808@hawaii.edu>
2022-09-06 13:15:52 -1000

    work in progress

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import { PAGE_IDS } from '../utilities/PageIDs';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
 
 /** Render a Not Found page if the user enters a URL that doesn't match any route. */
 const Profile = () => (
-  <Container >
-
+  <Container>
+    <Row className="justify-content-center">
+      <Col xs={5}>
+        <Col className="text-center"><h2>Profile</h2></Col>
+        <Card>
+          <Card.Body>
+            <TextField name="name" />
+            <NumField name="quantity" decimal={null} />
+            <SelectField name="condition" />
+            <SubmitField value="Submit" />
+            <ErrorsField />
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   </Container>
 );
 
