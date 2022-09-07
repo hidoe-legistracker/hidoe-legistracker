be8e2fa0d66ca1dbab745e0cfc47269f00ed8483
adamjparrilla <ajp808@hawaii.edu>
2022-09-06 14:23:26 -1000

    workin progress

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,25 +1,69 @@
 import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import { ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { AutoForm, ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { Stuffs } from '../../api/stuff/StuffCollection';
+import { defineMethod } from '../../api/base/BaseCollection.methods';
+import { PAGE_IDS } from '../utilities/PageIDs';
 
-/** Render a Not Found page if the user enters a URL that doesn't match any route. */
-const Profile = () => (
-  <Container>
-    <Row className="justify-content-center">
-      <Col xs={5}>
-        <Col className="text-center"><h2>Profile</h2></Col>
-        <Card>
-          <Card.Body>
-            <TextField name="name" />
-            <NumField name="quantity" decimal={null} />
-            <SelectField name="condition" />
-            <SubmitField value="Submit" />
-            <ErrorsField />
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-  </Container>
-);
+// Create a schema to specify the structure of the data to appear in the form.
+const formSchema = new SimpleSchema({
+  name: String,
+  quantity: Number,
+  condition: {
+    type: String,
+    allowedValues: ['excellent', 'good', 'fair', 'poor'],
+    defaultValue: 'good',
+  },
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+/* Renders the AddStuff page for adding a document. */
+const Profile = () => {
+
+  // On submit, insert the data.
+  const submit = (data, formRef) => {
+    const { name, quantity, condition } = data;
+    const owner = Meteor.user().username;
+    const collectionName = Stuffs.getCollectionName();
+    const definitionData = { name, quantity, condition, owner };
+    defineMethod.callPromise({ collectionName, definitionData })
+      .catch(error => swal('Error', error.message, 'error'))
+      .then(() => {
+        swal('Success', 'Item added successfully', 'success');
+        formRef.reset();
+      });
+  };
+
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
+  let fRef = null;
+  return (
+    <Container id={PAGE_IDS.PROFILE} className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={5}>
+          <Col className="text-center"><h2>Your Profile</h2></Col>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
+            <Card>
+              <Card.Body>
+                <Card.Title>First Name:</Card.Title>
+                <Card.Text>john</Card.Text>
+                <Card.Title>Last Name:</Card.Title>
+                <Card.Text>foo</Card.Text>
+                <Card.Title>Email:</Card.Title>
+                <Card.Text>john@foo.com</Card.Text>
+                <Card.Title>Employee ID:</Card.Title>
+                <Card.Text>012345678</Card.Text>
+              </Card.Body>
+            </Card>
+          </AutoForm>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Profile;
