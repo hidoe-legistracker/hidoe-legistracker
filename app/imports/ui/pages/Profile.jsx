d7a593a781f743cdbf03dd09fe8c909f9eae5d05
adamjparrilla <ajp808@hawaii.edu>
2022-09-06 21:07:41 -1000

    #7 mockup

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,69 +1,30 @@
 import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import { AutoForm } from 'uniforms-bootstrap5';
-import swal from 'sweetalert';
-import { Meteor } from 'meteor/meteor';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import SimpleSchema from 'simpl-schema';
-import { Stuffs } from '../../api/stuff/StuffCollection';
-import { defineMethod } from '../../api/base/BaseCollection.methods';
 import { PAGE_IDS } from '../utilities/PageIDs';
-
-// Create a schema to specify the structure of the data to appear in the form.
-const formSchema = new SimpleSchema({
-  name: String,
-  quantity: Number,
-  condition: {
-    type: String,
-    allowedValues: ['excellent', 'good', 'fair', 'poor'],
-    defaultValue: 'good',
-  },
-});
-
-const bridge = new SimpleSchema2Bridge(formSchema);
 
 /* Renders the Profile page for viewing your profile. */
 const Profile = () => {
 
-  const profilePic = { float: 'left', width: '50px' };
+  const profilePic = { float: 'right', width: '200px' };
 
-  // On submit, insert the data.
-  const submit = (data, formRef) => {
-    const { name, quantity, condition } = data;
-    const owner = Meteor.user().username;
-    const collectionName = Stuffs.getCollectionName();
-    const definitionData = { name, quantity, condition, owner };
-    defineMethod.callPromise({ collectionName, definitionData })
-      .catch(error => swal('Error', error.message, 'error'))
-      .then(() => {
-        swal('Success', 'Item added successfully', 'success');
-        formRef.reset();
-      });
-  };
-
-  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
-  let fRef = null;
   return (
     <Container id={PAGE_IDS.PROFILE} className="py-3">
       <Row className="justify-content-center">
         <Col xs={5}>
           <Col className="text-center"><h2>Your Profile</h2></Col>
-          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
-            <Card>
-              <Card.Body>
-                <Card.Title>Profile Picture:</Card.Title>
-                <Card.Img variant="top" style={profilePic} src="https://www.pbshawaii.org/wp-content/uploads/2016/03/hawaii-doe.png" alt="Hawaii DOE Logo" />
-                <Card.Title>First Name:</Card.Title>
-                <Card.Text>john</Card.Text>
-                <Card.Title>Last Name:</Card.Title>
-                <Card.Text>foo</Card.Text>
-                <Card.Title>Email:</Card.Title>
-                <Card.Text>john@foo.com</Card.Text>
-                <Card.Title>Employee ID:</Card.Title>
-                <Card.Text>012345678</Card.Text>
-              </Card.Body>
-            </Card>
-          </AutoForm>
+          <Card>
+            <Card.Body>
+              <Card.Img style={profilePic} src="https://www.pbshawaii.org/wp-content/uploads/2016/03/hawaii-doe.png" alt="Hawaii DOE Logo" />
+              <Card.Title>First Name:</Card.Title>
+              <Card.Text>john</Card.Text>
+              <Card.Title>Last Name:</Card.Title>
+              <Card.Text>foo</Card.Text>
+              <Card.Title>Email:</Card.Title>
+              <Card.Text>john@foo.com</Card.Text>
+              <Card.Title>Employee ID:</Card.Title>
+              <Card.Text>012345678</Card.Text>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container>
