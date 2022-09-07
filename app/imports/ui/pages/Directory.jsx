13f82d40859739b648ac83af3e647edf0b368a09
Edalaine Cadiena <ecadiena323@gmail.com>
2022-09-06 14:27:47 -1000

    #8 finish directory mockup, delete comments

--- app/imports/ui/pages/Directory.jsx
+++ app/imports/ui/pages/Directory.jsx
@@ -3,10 +3,6 @@ import { Col, Container, Row, Table } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Accordion from 'react-bootstrap/Accordion';
 import { PAGE_IDS } from '../utilities/PageIDs';
-// import { useTracker } from 'meteor/react-meteor-data';
-// import { Stuffs } from '../../api/stuff/StuffCollection';
-// import StuffItem from '../components/StuffItem';
-// import LoadingSpinner from '../components/LoadingSpinner';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const Directory = () => (
@@ -121,22 +117,5 @@ const Directory = () => (
     </Row>
   </Container>
 );
-// useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-/*
-  const { ready, stuffs } = useTracker(() => {
-    // Note that this subscription will get cleaned up
-    // when your component is unmounted or deps change.
-    // Get access to Stuff documents.
-    // const subscription = Stuffs.subscribeStuff();
-    // Determine if the subscription is ready
-    // const rdy = subscription.ready();
-    // Get the Stuff documents
-    // const stuffItems = Stuffs.find({}, { sort: { name: 1 } }).fetch();
-    return {
-      stuffs: stuffItems,
-      ready: rdy,
-    };
-  }, []);
-  */
 
 export default Directory;
