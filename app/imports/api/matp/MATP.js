338a0ec2bc875995dbda633b66cfcc81c0cf7fe0
ianbm <ianbm@hawaii.edu>
2022-09-06 14:24:18 -1000

    added define, publish, and subscribe methods for emails collection.

--- app/imports/api/matp/MATP.js
+++ app/imports/api/matp/MATP.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../stuff/StuffCollection';
 import { AdminProfiles } from '../user/AdminProfileCollection';
 import { UserProfiles } from '../user/UserProfileCollection';
+import { Emails } from '../email/EmailCollection';
 
 class MATPClass {
   collections;
@@ -16,6 +17,7 @@ class MATPClass {
       AdminProfiles,
       Stuffs,
       UserProfiles,
+      Emails,
     ];
     /*
      * A list of collection class instances in the order required for them to be sequentially loaded from a file.
