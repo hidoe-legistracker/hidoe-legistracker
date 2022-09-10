fa80a79ac3db858fc31181ed1ac11ac9966ce963
Edalaine Cadiena <ecadiena323@gmail.com>
2022-09-09 16:32:33 -1000

    #22 finished testimony page mockup

--- app/imports/ui/pages/Testimony.jsx
+++ app/imports/ui/pages/Testimony.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 // import { useTracker } from 'meteor/react-meteor-data';
 // import { Stuffs } from '../../api/stuff/StuffCollection';
 // import StuffItem from '../components/StuffItem';
@@ -26,14 +26,25 @@ const { ready, stuffs } = useTracker(() => {
  */
 
 const Testimony = () => (
-
   <Container id={PAGE_IDS.VIEW_TESTIMONY} className="view-testimony-container">
+    <Container>
+      <Row>
+        <Col>
+          <Button variant="secondary" size="sm" className="bill-button-spacing">
+            Edit
+          </Button>
+          <Button variant="secondary" size="sm" className="bill-button-spacing">
+            Print on Letterhead
+          </Button>
+        </Col>
+      </Row>
+    </Container>
     <Row className="row-center">
       <Col>
-        <Row className="testimony-header">Date: </Row>
-        <Row className="testimony-header">Time: </Row>
-        <Row className="testimony-header">Location: </Row>
-        <Row className="testimony-header">Committee: </Row>
+        <Row className="testimony-header1">Date: </Row>
+        <Row className="testimony-header1">Time: </Row>
+        <Row className="testimony-header1">Location: </Row>
+        <Row className="testimony-header1">Committee: </Row>
       </Col>
       <Col style={{ padding: 0 }}>
         <Row>04/05/2006</Row>
@@ -41,6 +52,42 @@ const Testimony = () => (
         <Row>3412 via Videoconference</Row>
         <Row>House of Finance</Row>
       </Col>
+      <Row className="row-center">
+        <Col xs={2} style={{ marginLeft: 20 }}>
+          <Row className="testimony-header2">Department: </Row>
+          <Row className="testimony-header2">Testifier: </Row>
+          <Row className="testimony-header2">Title of Bill: </Row>
+          <Row className="testimony-header2">Purpose of Bill: </Row>
+        </Col>
+        <Col>
+          <Row style={{ marginBottom: 20 }}>Education</Row>
+          <Row style={{ marginBottom: 20 }}>Jane Doe, Superintendent of Education</Row>
+          <Row style={{ marginBottom: 20 }}>SBC 13134798</Row>
+          <Row style={{ marginBottom: 20 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Row>
+        </Col>
+        <Container>
+          <Col>
+            <Row className="testimony-header2">Department Position: </Row>
+          </Col>
+          <Col>
+            <Row style={{ marginBottom: 20 }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Row>
+          </Col>
+        </Container>
+      </Row>
     </Row>
   </Container>
 );
