c50d881a3c82a747151e5c6e7beff04cef6e0d1c
adamjparrilla <ajp808@hawaii.edu>
2022-09-12 22:18:55 -1000

    #12
    done w/mockup

--- app/imports/ui/pages/CreateEmail.jsx
+++ app/imports/ui/pages/CreateEmail.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import { DateField, LongTextField, TextField } from 'uniforms-bootstrap5';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* Renders the CreateEmail page for composing emails. */
@@ -8,44 +7,59 @@ const CreateEmail = () => (
   <Container id={PAGE_IDS.CREATE_EMAIL} className="py-3">
     <Row className="justify-content-center">
       <h1>Create Email</h1>
-      <Row>--------------</Row>
       <Container>
         <Row>
           <Col>
-            <Button href="/" variant="secondary" size="sm">Address</Button>
-            <Button href="/" variant="secondary" size="sm">Check Names</Button>
+            <Button href="/" variant="secondary" size="sm">Address</Button>{' '}
+            <Button href="/" variant="secondary" size="sm">Check Names</Button>{' '}
           </Col>
         </Row>
       </Container>
-      <Row>
-        <div>
-          <TextField name="to" label="To: *" />
-        </div>
-        <div>
-          <TextField name="cc" label="cc: " />
-        </div>
-        <div>
-          <TextField name="bcc" label="bcc: " />
-        </div>
-        <div>
-          <TextField name="profile-name" label="From: *" />
-        </div>
-        <div>
-          <DateField name="date" label="Date: *" />
-        </div>
-      </Row>
-      <Row>--------------</Row>
-      <Row>
-        <TextField name="subject" label="Subject: " />
-      </Row>
-      <Row>
-        <LongTextField name="body" label="" />
-      </Row>
-      <Row>
-        <Col>
-          <Button style={{ marginRight: 2 }} variant="secondary" size="sm">Send</Button>
-        </Col>
-      </Row>
+      <Container>
+        <hr />
+      </Container>
+      <Form>
+        <Form.Group className="to">
+          <Form.Label>To: *</Form.Label>
+          <Form.Control type="to" placeholder="" />
+        </Form.Group>
+        <Form.Group className="cc">
+          <Form.Label>Cc: </Form.Label>
+          <Form.Control type="cc" placeholder="" />
+        </Form.Group>
+        <Form.Group className="bcc">
+          <Form.Label>Bcc: </Form.Label>
+          <Form.Control type="bcc" placeholder="" />
+        </Form.Group>
+        <Form.Group className="from">
+          <Form.Label>From: </Form.Label>
+          <Form.Control plaintext readOnly defaultValue="john foo/OSIP/HIDOE" />
+        </Form.Group>
+        <Form.Group className="date">
+          <Form.Label>Date: </Form.Label>
+          <Form.Control type="date" placeholder="" />
+        </Form.Group>
+      </Form>
+      <Container>
+        <hr />
+      </Container>
+      <Form>
+        <Form.Group className="subject">
+          <Form.Label>Subject: </Form.Label>
+          <Form.Control type="subject" placeholder="" />
+        </Form.Group>
+        <Form.Group className="body">
+          <Form.Label> </Form.Label>
+          <Form.Control type="body" as="textarea" rows={5} />
+        </Form.Group>
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label />
+          <Form.Control type="file" />
+        </Form.Group>
+      </Form>
+      <Col>
+        <Button href="/" variant="primary" size="lg">Send</Button>{' '}
+      </Col>
     </Row>
   </Container>
 );
