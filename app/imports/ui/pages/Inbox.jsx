a9de25f9751205c8296917b5941c1c75dafc2ad1
larissa-tsai <70196126+larissa-tsai@users.noreply.github.com>
2022-09-13 00:31:32 -1000

    #19

--- app/imports/ui/pages/Inbox.jsx
+++ app/imports/ui/pages/Inbox.jsx
@@ -1,33 +1,162 @@
 import React from 'react';
-import { Container, Tab, Row, Col, Nav } from 'react-bootstrap';
-import InboxItem from '../components/InboxItem';
+import { Container, Button } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
 import { PAGE_IDS } from '../utilities/PageIDs';
+// import InboxItem from '../components/InboxItem';
+// import Button from 'react-bootstrap/Button';
 
 /** Render a Not Found page if the user enters a URL that doesn't match any route. */
 const Inbox = () => (
-  <Container id={PAGE_IDS.INBOX} style={{ marginTop: '50px' }}>
-    <h1 style={{ textAlign: 'left' }}>Your Mail</h1>
-    <Tab.Container defaultActiveKey="inbox">
-      <Row>
-        <Col xs={3}>
-          <Nav variant="pills" className="flex-column">
-            <Nav.Item>
-              <Nav.Link eventKey="inbox">Inbox</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="unread">Unread</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Col>
-        <Col>
-          <Tab.Content>
-            <Tab.Pane eventKey="inbox">
-              <InboxItem item={{ from: 'John Foo', subject: 'Testimony Added', time: '8:50 PM' }} />
-            </Tab.Pane>
-          </Tab.Content>
-        </Col>
-      </Row>
-    </Tab.Container>
+  <Container id={PAGE_IDS.DIRECTORY} className="py-3">
+    <Row className="justify-content-center">
+      <h3 align="center">Bill Updates</h3>
+      <Col xs={10}>
+        <Row>
+          <div className="bill-table-header">
+            <Row>
+              <Col sm="auto">Select</Col>
+              <Col sm="auto">Date Updated</Col>
+              <Col sm="auto">Bill #</Col>
+              <Col sm="auto">Bill</Col>
+              <Col sm="auto">Status</Col>
+            </Row>
+          </div>
+          <div className="d-grid gap-2">
+            <Link className="bill-table" to="/view-bill">
+              <Row>
+                <Col sm="auto">
+                  <Form>
+                    <Form.Check
+                      inline
+                    />
+                  </Form>
+                </Col>
+                <Col sm="auto">12/12/2020</Col>
+                <Col sm="auto">1234</Col>
+                <Col sm="auto">...</Col>
+                <Col sm="auto">1st Crossover</Col>
+                <Col sm="auto">
+                  <Button size="sm" className="bill-button-spacing">
+                    Mark as Read
+                  </Button>
+                </Col>
+                <Col sm="auto">
+                  <Button variant="danger" size="sm" className="bill-button-spacing">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                      <path
+                        fillRule="evenodd"
+                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1
+                      1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                      />
+                    </svg>
+                  </Button>
+                </Col>
+              </Row>
+            </Link>
+            <Link className="bill-table" to="/view-bill">
+              <Row>
+                <Col sm="auto">
+                  <Form>
+                    <Form.Check
+                      inline
+                    />
+                  </Form>
+                </Col>
+                <Col sm="auto">12/12/2020</Col>
+                <Col sm="auto">1234</Col>
+                <Col sm="auto">...</Col>
+                <Col sm="auto">1st Crossover</Col>
+                <Col sm="auto">
+                  <Button size="sm" className="bill-button-spacing">
+                    Mark as Read
+                  </Button>
+                </Col>
+                <Col sm="auto">
+                  <Button variant="danger" size="sm" className="bill-button-spacing">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                      <path
+                        fillRule="evenodd"
+                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1
+                      1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                      />
+                    </svg>
+                  </Button>
+                </Col>
+              </Row>
+            </Link>
+            <Link className="bill-table" to="/view-bill">
+              <Row>
+                <Col sm="auto">
+                  <Form>
+                    <Form.Check
+                      inline
+                    />
+                  </Form>
+                </Col>
+                <Col sm="auto">12/12/2020</Col>
+                <Col sm="auto">1234</Col>
+                <Col sm="auto">...</Col>
+                <Col sm="auto">1st Crossover</Col>
+                <Col sm="auto">
+                  <Button size="sm" className="bill-button-spacing">
+                    Mark as Read
+                  </Button>
+                </Col>
+                <Col sm="auto">
+                  <Button variant="danger" size="sm" className="bill-button-spacing">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                      <path
+                        fillRule="evenodd"
+                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1
+                      1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                      />
+                    </svg>
+                  </Button>
+                </Col>
+              </Row>
+            </Link>
+            <Link className="bill-table" to="/view-bill">
+              <Row>
+                <Col sm="auto">
+                  <Form>
+                    <Form.Check
+                      inline
+                    />
+                  </Form>
+                </Col>
+                <Col sm="auto">12/12/2020</Col>
+                <Col sm="auto">1234</Col>
+                <Col sm="auto">...</Col>
+                <Col sm="auto">1st Crossover</Col>
+                <Col sm="auto">
+                  <Button size="sm" className="bill-button-spacing">
+                    Mark as Read
+                  </Button>
+                </Col>
+                <Col sm="auto">
+                  <Button variant="danger" size="sm" className="bill-button-spacing">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                      <path
+                        fillRule="evenodd"
+                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1
+                      1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                      />
+                    </svg>
+                  </Button>
+                </Col>
+              </Row>
+            </Link>
+          </div>
+        </Row>
+      </Col>
+    </Row>
   </Container>
 );
 
