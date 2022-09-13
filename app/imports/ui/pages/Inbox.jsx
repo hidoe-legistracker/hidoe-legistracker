c7c66c9da990754061b1bbd210c1a20438d6f2ec
Ethan Chee <ethancheez@gmail.com>
2022-09-12 15:39:39 -1000

    Update Inbox so testcafe passes

--- app/imports/ui/pages/Inbox.jsx
+++ app/imports/ui/pages/Inbox.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-// import { PAGE_IDS } from '../utilities/PageIDs';
+import { PAGE_IDS } from '../utilities/PageIDs';
 
 /** Render a Not Found page if the user enters a URL that doesn't match any route. */
 const Inbox = () => (
-  <Container />
+  <Container id={PAGE_IDS.INBOX} />
 );
 
 export default Inbox;
