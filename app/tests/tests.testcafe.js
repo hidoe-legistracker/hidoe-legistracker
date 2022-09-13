4d8a2df17abdfc9eb238b5f20f1e082719ce12c2
Ethan Chee <ethancheez@gmail.com>
2022-09-12 15:29:43 -1000

    Update test scripts to be more specific

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -11,10 +11,10 @@ const credentials = { username: 'john@foo.com', password: 'changeme' };
 const adminCredentials = { username: 'admin@foo.com', password: 'changeme' };
 // const newCredentials = { username: 'jane@foo.com', password: 'changeme' };
 
-fixture('meteor-application-template-production localhost test with default db')
+fixture('hidoe-legistracker localhost test with default db')
   .page('http://localhost:3000');
 
-test('Test that landing page shows up', async () => {
+test('Test that landing / sign in page shows up', async () => {
   await signInPage.isDisplayed();
 });
 
@@ -26,42 +26,42 @@ test('Test that signin and signout work', async () => {
   await signOutPage.isDisplayed();
 });
 
-test('Test that user pages show up', async () => {
+test('Test Directory Page', async () => {
   await navBar.gotoSignInPage();
   await signInPage.signin(credentials.username, credentials.password);
   await navBar.isLoggedIn(credentials.username);
-  // await navBar.gotoDirectoryPage();
-  // await directoryPage.isDisplayed();
-  // await navBar.gotoMyFoldersPage();
-  // await myFoldersPage.isDisplayed();
-  // await navBar.gotoInboxPage();
-  // await inboxPage.isDisplayed();
-  // want to see if we can get to the editStuffPage
-  // const editLinks = await Selector(`.${COMPONENT_IDS.LIST_STUFF_EDIT}`);
-  // await t.click(editLinks.nth(0));
-  // await editStuffPage.isDisplayed();
+  await navBar.gotoDirectoryPage();
+  await directoryPage.isDisplayed();
   await navBar.logout();
   await signOutPage.isDisplayed();
 });
 
-test('Test that admin pages show up', async () => {
+test('Test My Folders Page', async () => {
+  await navBar.gotoSignInPage();
+  await signInPage.signin(credentials.username, credentials.password);
+  await navBar.isLoggedIn(credentials.username);
+  await navBar.gotoMyFoldersPage();
+  await myFoldersPage.isDisplayed();
+  await navBar.logout();
+  await signOutPage.isDisplayed();
+});
+
+test('Test Inbox Page', async () => {
+  await navBar.gotoSignInPage();
+  await signInPage.signin(credentials.username, credentials.password);
+  await navBar.isLoggedIn(credentials.username);
+  await navBar.gotoInboxPage();
+  await inboxPage.isDisplayed();
+  await navBar.logout();
+  await signOutPage.isDisplayed();
+});
+
+test('Test Create Measure Page', async () => {
   await navBar.gotoSignInPage();
   await signInPage.signin(adminCredentials.username, adminCredentials.password);
   await navBar.isLoggedIn(adminCredentials.username);
-  await navBar.gotoDirectoryPage();
-  await directoryPage.isDisplayed();
-  await navBar.gotoMyFoldersPage();
-  await myFoldersPage.isDisplayed();
   await navBar.gotoCreateMeasurePage();
   await createMeasurePage.isDisplayed();
-  await navBar.gotoInboxPage();
-  await inboxPage.isDisplayed();
-  // want to see if we can get to the editStuffPage
-  // const editLinks = await Selector(`.${COMPONENT_IDS.LIST_STUFF_EDIT}`);
-  // await t.click(editLinks.nth(0));
-  // await editStuffPage.isDisplayed();
-  // await navBar.gotoListStuffAdminPage();
-  // await listStuffAdminPage.isDisplayed();
-  // await navBar.gotoManageDatabasePage();
-  // await manageDatabasePage.isDisplayed();
+  await navBar.logout();
+  await signOutPage.isDisplayed();
 });
