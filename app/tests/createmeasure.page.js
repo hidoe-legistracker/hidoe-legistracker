2d773676003adf5ab4bd1f0c50a9eaae94b2312f
Ethan Chee <ethancheez@gmail.com>
2022-09-13 19:51:02 -1000

    Update createmeasure.page.js

--- app/tests/createmeasure.page.js
+++ app/tests/createmeasure.page.js
@@ -32,7 +32,7 @@ class CreateMeasurePage {
     await t.click(actionSelector).click(actionOption.withText('Testimony')).expect(actionSelector.value).eql('Testimony');
 
     await t.click(`#${COMPONENT_IDS.CREATE_MEASURE_FORM_SUBMIT}`);
-    await t.expect(Selector(`#${COMPONENT_IDS.CREATE_MEASURE_FORM_CONFIRM}`).exists).ok();
+    await t.expect(Selector(`#${COMPONENT_IDS.CREATE_MEASURE_FORM_CONFIRM}`).visible).ok();
     await t.click(`#${COMPONENT_IDS.CREATE_MEASURE_FORM_CONFIRM}`);
   }
 }
