a6687b613dfe5c5765b8329aea3a4ee7ddad9d84
coleamparo <camparo@hawaii.edu>
2022-09-20 14:19:19 -1000

    #103
    The bills in MyFolder link to ViewBill MockupPage.

--- app/imports/ui/pages/MyFolders.jsx
+++ app/imports/ui/pages/MyFolders.jsx
@@ -6,7 +6,7 @@ import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
 import { Link } from 'react-router-dom';
 import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
+import Tabs from 'react-bootstrap/Tabs';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /* Renders a drop down menu  that has a collection of bills that are favorited */
@@ -28,110 +28,126 @@ const MyFolders = () => (
             </Nav.Item>
           </Nav>
         </Col>
-        <Col sm={9}>
+        <Col sm={9} className="bill-section">
           <Tab.Content>
             <Tab.Pane eventKey="first">
-              <Col xs={15} className="bill-section">
-                <Button variant="secondary" size="sm" className="bill-button-spacing">
-                  View Bill by Status
-                </Button>
-                <Button variant="secondary" size="sm" className="bill-button-spacing">
-                  Create Tracking Document
-                </Button>
-                <Button variant="secondary" size="sm" className="bill-button-spacing">
-                  Send DocLink
-                </Button>
-                <Button variant="secondary" size="sm" className="bill-button-spacing">
-                  Export to XL
-                </Button>
-                <Row className="m-2">
-                  <Table>
-                    <thead>
-                      <tr>
-                        <th scope="col">Bill #</th>
-                        <th scope="col">Bill</th>
-                        <th scope="col">Office</th>
-                        <th scope="col">Action</th>
-                        <th scope="col">Rationale</th>
-                        <th scope="col">Committee</th>
-                        <th scope="col">Hearing</th>
-                        <th scope="col">Type</th>
-                        <th scope="col">Position</th>
-                        <th scope="col">Testifier</th>
-                        <th scope="col">Status</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <Link className="table-row" to="/view-bill">
-                        <th scope="row">1234</th>
-                        <td>.....</td>
-                        <td>OCID BOE</td>
-                        <td>Testimony</td>
-                        <td>........</td>
-                        <td>EDU, FIN</td>
-                        <td>12/02/2022</td>
-                        <td>Hearing</td>
-                        <td>Support</td>
-                        <td>John Doe</td>
-                        <td>
-                          <ProgressBar now={billProgress} label={`${billProgress}`} visuallyHidden />
-                        </td>
-                      </Link>
-                    </tbody>
-                  </Table>
-                </Row>
+              <Col xs={12}>
+                <Tabs defaultActiveKey="all-bills" id="fill-tab-example" className="mb-3" fill>
+                  <Tab eventKey="all-bills" title="All Bills">
+                    <Row>
+                      <Table>
+                        <thead>
+                          <tr>
+                            <th scope="col">Bill #</th>
+                            <th scope="col">Bill</th>
+                            <th scope="col">Office</th>
+                            <th scope="col">Action</th>
+                            <th scope="col">Rationale</th>
+                            <th scope="col">Committee</th>
+                            <th scope="col">Hearing</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Position</th>
+                            <th scope="col">Testifier</th>
+                            <th scope="col">Status</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <Link className="table-row" to="/view-bill">
+                            <th scope="row">1234</th>
+                            <td>.....</td>
+                            <td>OCID BOE</td>
+                            <td>Testimony</td>
+                            <td>........</td>
+                            <td>EDU, FIN</td>
+                            <td>12/02/2022</td>
+                            <td>Hearing</td>
+                            <td>Support</td>
+                            <td>John Doe</td>
+                            <td>
+                              <ProgressBar now={billProgress} label={`${billProgress}`} visuallyHidden />
+                            </td>
+                          </Link>
+                        </tbody>
+                      </Table>
+                    </Row>
+                  </Tab>
+                  <Tab eventKey="inactive-bills" title="Inactive Bills">
+                    ...
+                  </Tab>
+                  <Tab eventKey="actions" title="Actions">
+                    <Nav variant="pills" className="flex-column">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Monitor</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Testimony</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                  </Tab>
+                  <Tab eventKey="hearings" title="Hearings">
+                    ...
+                  </Tab>
+                </Tabs>
               </Col>
             </Tab.Pane>
             <Tab.Pane eventKey="second">
-              <Col xs={15} className="bill-section">
-                <Button variant="secondary" size="sm" className="bill-button-spacing">
-                  View Bill by Status
-                </Button>
-                <Button variant="secondary" size="sm" className="bill-button-spacing">
-                  Create Tracking Document
-                </Button>
-                <Button variant="secondary" size="sm" className="bill-button-spacing">
-                  Send DocLink
-                </Button>
-                <Button variant="secondary" size="sm" className="bill-button-spacing">
-                  Export to XL
-                </Button>
-                <Row className="m-2">
-                  <Table>
-                    <thead>
-                      <tr>
-                        <th scope="col">Bill #</th>
-                        <th scope="col">Bill</th>
-                        <th scope="col">Office</th>
-                        <th scope="col">Action</th>
-                        <th scope="col">Rationale</th>
-                        <th scope="col">Committee</th>
-                        <th scope="col">Hearing</th>
-                        <th scope="col">Type</th>
-                        <th scope="col">Position</th>
-                        <th scope="col">Testifier</th>
-                        <th scope="col">Status</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <Link className="table-row" to="/view-bill">
-                        <th scope="row">1234</th>
-                        <td>.....</td>
-                        <td>OCID BOE</td>
-                        <td>Testimony</td>
-                        <td>........</td>
-                        <td>EDU, FIN</td>
-                        <td>12/02/2022</td>
-                        <td>Hearing</td>
-                        <td>Support</td>
-                        <td>John Doe</td>
-                        <td>
-                          <ProgressBar now={billProgress} label={`${billProgress}`} visuallyHidden />
-                        </td>
-                      </Link>
-                    </tbody>
-                  </Table>
-                </Row>
+              <Col xs={12}>
+                <Tabs defaultActiveKey="all-bills" id="fill-tab-example" className="mb-3" fill>
+                  <Tab eventKey="all-bills" title="All Bills">
+                    <Row>
+                      <Table>
+                        <thead>
+                          <tr>
+                            <th scope="col">Bill #</th>
+                            <th scope="col">Bill</th>
+                            <th scope="col">Office</th>
+                            <th scope="col">Action</th>
+                            <th scope="col">Rationale</th>
+                            <th scope="col">Committee</th>
+                            <th scope="col">Hearing</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Position</th>
+                            <th scope="col">Testifier</th>
+                            <th scope="col">Status</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <Link className="table-row" to="/view-bill">
+                            <th scope="row">1234</th>
+                            <td>.....</td>
+                            <td>OCID BOE</td>
+                            <td>Testimony</td>
+                            <td>........</td>
+                            <td>EDU, FIN</td>
+                            <td>12/02/2022</td>
+                            <td>Hearing</td>
+                            <td>Support</td>
+                            <td>John Doe</td>
+                            <td>
+                              <ProgressBar now={billProgress} label={`${billProgress}`} visuallyHidden />
+                            </td>
+                          </Link>
+                        </tbody>
+                      </Table>
+                    </Row>
+                  </Tab>
+                  <Tab eventKey="inactive-bills" title="Inactive Bills">
+                    ...
+                  </Tab>
+                  <Tab eventKey="actions" title="Actions">
+                    <Nav variant="pills" className="flex-column">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Monitor</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Testimony</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                  </Tab>
+                  <Tab eventKey="hearings" title="Hearings">
+                    ...
+                  </Tab>
+                </Tabs>
               </Col>
             </Tab.Pane>
           </Tab.Content>
