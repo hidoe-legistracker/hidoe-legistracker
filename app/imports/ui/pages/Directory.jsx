c3624e3a96ce332e1385b7155a6d15c3838eceda
Edalaine Cadiena <ecadiena323@gmail.com>
2022-09-22 13:55:58 -1000

    #90

--- app/imports/ui/pages/Directory.jsx
+++ app/imports/ui/pages/Directory.jsx
@@ -34,111 +34,88 @@ MeasureComponent.propTypes = {
 };
 
 /* Renders a table containing all of the Measure documents. */
-class Directory extends React.Component {
-  renderPage() {
-    const projects = _.pluck(Measure.collection.find().fetch(), 'name');
-    const projectData = projects.map(project => getProjectData(project));
-
-    return (
-      <Container id={PAGE_IDS.DIRECTORY} className="py-3">
-        <Row className="justify-content-center">
-          <Col className="folder-section">
-            <h6 align="center" style={{ marginBottom: 20 }}>Legislative Tracking System 2022</h6>
+const Directory = () => (
+  <Container id={PAGE_IDS.DIRECTORY} className="py-3">
+    <Row className="justify-content-center">
+      <Col className="folder-section">
+        <h6 align="center" style={{ marginBottom: 20 }}>Legislative Tracking System 2022</h6>
+        <Nav variant="pills" className="flex-column">
+          <Nav.Item>
+            <Nav.Link eventKey="first">BOE</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="second">Deputy</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="third">OCID</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="fourth">OFO</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="fifth">OFS</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="sixth">OHE</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="seventh">OITS</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="eighth">OSIP</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="ninth">OSSS</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="tenth">OTM</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="eleventh">Supt</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Col>
+      <Col xs={10}>
+        <Tabs defaultActiveKey="all-bills" id="fill-tab-example" className="mb-3" fill>
+          <Tab eventKey="all-bills" title="All Bills">
+            <Row>
+              <Table>
+                <thead>
+                  <tr>
+                    <th scope="col">Bill #</th>
+                    <th scope="col">Bill</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Offices</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {_.map(<Measure measure={measure} />)}
+                </tbody>
+              </Table>
+            </Row>
+          </Tab>
+          <Tab eventKey="inactive-bills" title="Inactive Bills">
+            ...
+          </Tab>
+          <Tab eventKey="actions" title="Actions">
             <Nav variant="pills" className="flex-column">
               <Nav.Item>
-                <Nav.Link eventKey="first">BOE</Nav.Link>
+                <Nav.Link eventKey="first">Monitor</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link eventKey="second">Deputy</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="third">OCID</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="fourth">OFO</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="fifth">OFS</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="sixth">OHE</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="seventh">OITS</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="eighth">OSIP</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="ninth">OSSS</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="tenth">OTM</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="eleventh">Supt</Nav.Link>
+                <Nav.Link eventKey="second">Testimony</Nav.Link>
               </Nav.Item>
             </Nav>
-          </Col>
-          <Col xs={10}>
-            <Tabs defaultActiveKey="all-bills" id="fill-tab-example" className="mb-3" fill>
-              <Tab eventKey="all-bills" title="All Bills">
-                <Row>
-                  <Table>
-                    <thead>
-                      <tr>
-                        <th scope="col">Bill #</th>
-                        <th scope="col">Bill</th>
-                        <th scope="col">Description</th>
-                        <th scope="col">Offices</th>
-                        <th scope="col">Type</th>
-                        <th scope="col">Status</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {_.map(<MeasureComponent measure={measure} />)}
-                    </tbody>
-                  </Table>
-                </Row>
-              </Tab>
-              <Tab eventKey="inactive-bills" title="Inactive Bills">
-                ...
-              </Tab>
-              <Tab eventKey="actions" title="Actions">
-                <Nav variant="pills" className="flex-column">
-                  <Nav.Item>
-                    <Nav.Link eventKey="first">Monitor</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="second">Testimony</Nav.Link>
-                  </Nav.Item>
-                </Nav>
-              </Tab>
-              <Tab eventKey="hearings" title="Hearings">
-                ...
-              </Tab>
-            </Tabs>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+          </Tab>
+          <Tab eventKey="hearings" title="Hearings">
+            ...
+          </Tab>
+        </Tabs>
+      </Col>
+    </Row>
+  </Container>
+);
 
-  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
-  render() {
-    return (this.props.ready) ? this.renderPage() : <LoadingSpinner />;
-  }
-}
-
-Directory.propTypes = {
-  ready: PropTypes.bool.isRequired,
-};
-
-/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-export default withTracker(() => {
-  // Ensure that minimongo is populated with all collections prior to running render().
-  const sub = Meteor.subscribe(Measure.name);
-  return {
-    ready: sub.ready(),
-  };
-})(Directory);
+export default Directory;
