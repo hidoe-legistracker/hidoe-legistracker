a79a14f3267d978db0723d1e5602967a999be6b4
ianbm <ianbm@hawaii.edu>
2022-09-22 12:27:05 -1000

    #98 displayed TestimonyCollection data on the view bill page

--- app/imports/ui/pages/ViewBill.jsx
+++ app/imports/ui/pages/ViewBill.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Col, Container, Row, Button, ProgressBar } from 'react-bootstrap';
 import { FileEarmarkText } from 'react-bootstrap-icons';
-// import { useTracker } from 'meteor/react-meteor-data';
+import { useTracker } from 'meteor/react-meteor-data';
 // import { Stuffs } from '../../api/stuff/StuffCollection';
 // import StuffItem from '../components/StuffItem';
-// import LoadingSpinner from '../components/LoadingSpinner';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 import Table from 'react-bootstrap/Table';
+import LoadingSpinner from '../components/LoadingSpinner';
 import { PAGE_IDS } from '../utilities/PageIDs';
+import { Testimonies } from '../../api/testimony/TestimonyCollection';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row.
 *
@@ -33,136 +34,149 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 * */
 const billProgress = 60;
 
-const ViewBill = () => (
-  <Container id={PAGE_IDS.VIEW_BILL} className="view-bill-container">
-    <Container>
-      <Row>
+const ViewBill = () => {
+  const { ready, testimonies } = useTracker(() => {
+    const subscription = Testimonies.subscribeTestimony();
+    const rdy = subscription.ready();
+    const testimonyItems = Testimonies.find().fetch();
+    return {
+      ready: rdy,
+      testimonies: testimonyItems,
+    };
+  }, []);
+  return ready ? (
+    <Container id={PAGE_IDS.VIEW_BILL} className="view-bill-container">
+      <Container>
+        <Row>
+          <Col>
+            <Button href="/create-testimony" variant="secondary" size="sm" className="bill-button-spacing">
+              <FileEarmarkText style={{ marginRight: '0.5em', marginTop: '-5px' }} />
+              Create Testimony
+            </Button>
+            <Button variant="secondary" size="sm" className="bill-button-spacing" href="/monitoringreport">
+              <FileEarmarkText style={{ marginRight: '0.5em', marginTop: '-5px' }} />
+              Monitoring Report
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+      <h1>Bill #1234</h1>
+      <Row style={{ alignItems: 'center', justifyContent: 'center', marginLeft: 2 }}>
         <Col>
-          <Button href="/create-testimony" variant="secondary" size="sm" className="bill-button-spacing">
-            <FileEarmarkText style={{ marginRight: '0.5em', marginTop: '-5px' }} />
-            Create Testimony
-          </Button>
-          <Button variant="secondary" size="sm" className="bill-button-spacing" href="/monitoringreport">
-            <FileEarmarkText style={{ marginRight: '0.5em', marginTop: '-5px' }} />
-            Monitoring Report
-          </Button>
+          <Row style={{ fontWeight: 'bold' }}>Bill / Resolution</Row>
+          <Row>...............</Row>
         </Col>
       </Row>
-    </Container>
-    <h1>Bill #1234</h1>
-    <Row style={{ alignItems: 'center', justifyContent: 'center', marginLeft: 2 }}>
-      <Col>
-        <Row style={{ fontWeight: 'bold' }}>Bill / Resolution</Row>
-        <Row>...............</Row>
-      </Col>
-    </Row>
-    <Row style={{ alignContent: 'center', justifyContent: 'center', margin: 0 }}>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Office</Row>
-        <Row>OCID BOE</Row>
-      </Col>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Action</Row>
-        <Row>Testimony</Row>
-      </Col>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Act #</Row>
-        <Row>--</Row>
-      </Col>
-    </Row>
-    <Row style={{ alignContent: 'center', justifyContent: 'center', margin: 0 }}>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Rationale</Row>
-        <Row>..............</Row>
-      </Col>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Committee Referral</Row>
-        <Row>EDU, FIN</Row>
-      </Col>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Committee</Row>
-        <Row>Conference</Row>
-      </Col>
-    </Row>
-    <Row style={{ alignContent: 'center', justifyContent: 'center', margin: 0 }}>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>DOE Position</Row>
-        <Row>Support</Row>
-      </Col>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Testifier(s)</Row>
-        <Row>John Doe</Row>
-      </Col>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Status</Row>
-        <Row>1st Crossover</Row>
-      </Col>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Proposed</Row>
-        <Row>..........</Row>
-      </Col>
-    </Row>
-    <Row style={{ alignContent: 'center', justifyContent: 'center', margin: 0 }}>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Hearing Date</Row>
-        <Row>12/02/2022</Row>
-      </Col>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Hearing Time</Row>
-        <Row>12:00pm</Row>
-      </Col>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Hearing Location</Row>
-        <Row>CR 229</Row>
-      </Col>
-      <Col className="view-bill-columns">
-        <Row style={{ fontWeight: 'bold' }}>Hearing Type</Row>
-        <Row>Hearing</Row>
-      </Col>
-    </Row>
-    <Container className="view-testimony-container">
-      <h3>Testimonies</h3>
-      <Table>
-        <thead>
-          <tr>
-            <th scope="col">Hearing Date</th>
-            <th scope="col">Bill #</th>
-            <th scope="col">Prepared By</th>
-            <th scope="col">DOE Positon</th>
-            <th scope="col">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Link className="table-row" to="/view-testimony">
-            <th scope="row">04/05/2022</th>
-            <td>SB 123456</td>
-            <td>Jane Doe</td>
-            <td>Support</td>
-            <td>
-              <ProgressBar now={billProgress} label={`${billProgress}`} visuallyHidden />
-            </td>
-          </Link>
-        </tbody>
-      </Table>
-    </Container>
+      <Row style={{ alignContent: 'center', justifyContent: 'center', margin: 0 }}>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Office</Row>
+          <Row>OCID BOE</Row>
+        </Col>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Action</Row>
+          <Row>Testimony</Row>
+        </Col>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Act #</Row>
+          <Row>--</Row>
+        </Col>
+      </Row>
+      <Row style={{ alignContent: 'center', justifyContent: 'center', margin: 0 }}>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Rationale</Row>
+          <Row>..............</Row>
+        </Col>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Committee Referral</Row>
+          <Row>EDU, FIN</Row>
+        </Col>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Committee</Row>
+          <Row>Conference</Row>
+        </Col>
+      </Row>
+      <Row style={{ alignContent: 'center', justifyContent: 'center', margin: 0 }}>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>DOE Position</Row>
+          <Row>Support</Row>
+        </Col>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Testifier(s)</Row>
+          <Row>John Doe</Row>
+        </Col>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Status</Row>
+          <Row>1st Crossover</Row>
+        </Col>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Proposed</Row>
+          <Row>..........</Row>
+        </Col>
+      </Row>
+      <Row style={{ alignContent: 'center', justifyContent: 'center', margin: 0 }}>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Hearing Date</Row>
+          <Row>12/02/2022</Row>
+        </Col>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Hearing Time</Row>
+          <Row>12:00pm</Row>
+        </Col>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Hearing Location</Row>
+          <Row>CR 229</Row>
+        </Col>
+        <Col className="view-bill-columns">
+          <Row style={{ fontWeight: 'bold' }}>Hearing Type</Row>
+          <Row>Hearing</Row>
+        </Col>
+      </Row>
+      <Container className="view-testimony-container">
+        <h3>Testimonies</h3>
+        <Table>
+          <thead>
+            <tr>
+              <th scope="col">Hearing Date</th>
+              <th scope="col">Bill #</th>
+              <th scope="col">Prepared By</th>
+              <th scope="col">DOE Positon</th>
+              <th scope="col">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {testimonies.map(testimony => (
+              <Link className="table-row" to="/view-testimony">
+                <th scope="row">{testimony.hearingDate.toLocaleDateString()}</th>
+                <td>{testimony.billNumber}</td>
+                <td>{testimony.testifier}</td>
+                <td>{testimony.deptPosition}</td>
+                <td>
+                  <ProgressBar now={billProgress} label={`${billProgress}`} visuallyHidden />
+                </td>
+              </Link>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
 
-    <hr
-      style={{
-        background: 'black',
-        color: 'black',
-        borderColor: 'black',
-        height: '1px',
-      }}
-    />
-    <Row style={{ marginTop: 10 }}>
-      <Form>
-        <Form.Check
-          inline
-          label="I want to receive hearing notifications for this bill"
-        />
-      </Form>
-    </Row>
-  </Container>
-);
+      <hr
+        style={{
+          background: 'black',
+          color: 'black',
+          borderColor: 'black',
+          height: '1px',
+        }}
+      />
+      <Row style={{ marginTop: 10 }}>
+        <Form>
+          <Form.Check
+            inline
+            label="I want to receive hearing notifications for this bill"
+          />
+        </Form>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner message="Loading Data" />;
+};
 
 export default ViewBill;
