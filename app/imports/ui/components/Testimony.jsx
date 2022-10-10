c906d7b66edb40596cbae593cfa4f897963bb280
ianbm <ianbm@hawaii.edu>
2022-10-09 17:33:34 -1000

    #150
    Fixed TestimonyPage error when using browser back/forward/refresh buttons

--- app/imports/ui/components/Testimony.jsx
+++ app/imports/ui/components/Testimony.jsx
@@ -1,38 +1,12 @@
 import React from 'react';
-import { useParams } from 'react-router';
-import { useTracker } from 'meteor/react-meteor-data';
-import { Col, Container, Image, Row } from 'react-bootstrap';
-import { PAGE_IDS } from '../utilities/PageIDs';
-import { Testimonies } from '../../api/testimony/TestimonyCollection';
-import { Measures } from '../../api/measure/MeasureCollection';
-import LoadingSpinner from './LoadingSpinner';
+import { Col, Container, Row } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
 // used https://www.npmjs.com/package/react-to-print
-export const Testimony = React.forwardRef((props, ref) => {
-  const { _id } = useParams();
-  const { measure, testimony, ready } = useTracker(() => {
-    const testimonySubscription = Testimonies.subscribeTestimony();
-    const measureSubscription = Measures.subscribeMeasures();
-    const rdy = testimonySubscription.ready() && measureSubscription.ready();
-    const testimonyDoc = Testimonies.findDoc(_id);
-    const billNum = Number(testimonyDoc.billNumber);
-    const measureDoc = Measures.findOne({ measureNumber: billNum });
-    return {
-      measure: measureDoc,
-      testimony: testimonyDoc,
-      ready: rdy,
-    };
-  }, [_id]);
-  return ready ? (
+export const Testimony = React.forwardRef(({ testimony }, ref) => {
+  return (
     <div ref={ref}>
-      <Container id={PAGE_IDS.VIEW_TESTIMONY} className="view-testimony-container">
-        <Row className="row-center">
-          <Image src="https://upload.wikimedia.org/wikipedia/commons/d/d3/HSSC_Seal.png" className="logo" />
-          <Row className="letterhead" style={{ marginTop: 10 }}>STATE OF HAWAII</Row>
-          <Row className="letterhead">DEPARTMENT OF EDUCATION</Row>
-          <Row className="letterhead" style={{ fontWeight: 'normal' }}>P.O. BOX 2360</Row>
-          <Row className="letterhead" style={{ marginBottom: 20, fontWeight: 'normal' }}>HONOLULU, HAWAI`I 96894</Row>
-        </Row>
+      <Container>
         <Row className="row-center">
           <Col>
             <Row className="testimony-header1">Date: </Row>
@@ -41,41 +15,53 @@ export const Testimony = React.forwardRef((props, ref) => {
             <Row className="testimony-header1">Committee: </Row>
           </Col>
           <Col style={{ padding: 0 }}>
-            <Row>{testimony.hearingDate ? testimony.hearingDate.toLocaleDateString() : '-'}</Row>
-            <Row>{testimony.hearingDate ? testimony.hearingDate.toLocaleTimeString() : '-'}</Row>
-            <Row>{testimony.hearingLocation ? testimony.hearingLocation : '-'}</Row>
+            <Row>{testimony.hearingDate}</Row>
+            <Row>{testimony.hearingTime}</Row>
+            <Row>{testimony.hearingLocation}</Row>
             <Row>{testimony.committeeName}</Row>
           </Col>
           <Row className="row-center">
-            <Row>
-              <Col className="testimony-header2" xs={2}>Department</Col>
-              <Col>{testimony.representing ? testimony.representing : '-'}</Col>
-            </Row>
-            <Row>
-              <Col className="testimony-header2" xs={2}>Testifier</Col>
-              <Col>{testimony.testifier}</Col>
-            </Row>
-            <Row>
-              <Col className="testimony-header2" xs={2}>Title of Bill</Col>
-              <Col>{measure.measureType.toUpperCase()}{measure.measureNumber}</Col>
-            </Row>
-            <Row>
-              <Col className="testimony-header2" xs={2}>Purpose of Bill</Col>
-              <Col>{measure.description ? measure.description : '-'}</Col>
-            </Row>
-            <Container style={{ marginTop: 10 }}>
+            <Col xs={2} style={{ marginLeft: 20 }}>
+              <Row className="testimony-header2">Department: </Row>
+              <Row className="testimony-header2">Testifier: </Row>
+              <Row className="testimony-header2">Title of Bill: </Row>
+              <Row className="testimony-header2">Purpose of Bill: </Row>
+            </Col>
+            <Col>
+              <Row style={{ marginBottom: 20 }}>{testimony.department}</Row>
+              <Row style={{ marginBottom: 20 }}>{testimony.testifier}</Row>
+              <Row style={{ marginBottom: 20 }}>{testimony.measureTitle}</Row>
+              <Row style={{ marginBottom: 20 }}>{testimony.measureDescription}</Row>
+            </Col>
+            <Container>
               <Col>
                 <Row className="testimony-header2">Department Position: </Row>
               </Col>
               <Col>
                 <Row style={{ marginBottom: 20 }}>{testimony.introduction}</Row>
                 <Row style={{ marginBottom: 20 }}>{testimony.content}</Row>
-                <Row style={{ marginBottom: 20 }}>{testimony.closing ? testimony.closing : ''}</Row>
+                <Row style={{ marginBottom: 20 }}>{testimony.closing}</Row>
               </Col>
             </Container>
           </Row>
         </Row>
       </Container>
     </div>
-  ) : <LoadingSpinner />;
+  );
 });
+
+Testimony.propTypes = {
+  testimony: PropTypes.shape({
+    hearingDate: PropTypes.string,
+    hearingTime: PropTypes.string,
+    hearingLocation: PropTypes.string,
+    committeeName: PropTypes.string,
+    department: PropTypes.string,
+    testifier: PropTypes.string,
+    introduction: PropTypes.string,
+    content: PropTypes.string,
+    closing: PropTypes.string,
+    measureTitle: PropTypes.string,
+    measureDescription: PropTypes.string,
+  }).isRequired,
+};
--- app/imports/ui/pages/TestimonyPage.jsx
+++ app/imports/ui/pages/TestimonyPage.jsx
@@ -1,19 +1,53 @@
 import React, { useRef } from 'react';
 import { useParams } from 'react-router';
+import { useTracker } from 'meteor/react-meteor-data';
 import { useReactToPrint } from 'react-to-print';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Testimony } from '../components/Testimony';
+import { PAGE_IDS } from '../utilities/PageIDs';
+import { Testimonies } from '../../api/testimony/TestimonyCollection';
+import { Measures } from '../../api/measure/MeasureCollection';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 const TestimonyPage = () => {
   const componentRef = useRef();
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   });
-  const { _id } = useParams();
 
-  return (
+  const { _id } = useParams();
+  const { measures, testimony, ready } = useTracker(() => {
+    const testimonySubscription = Testimonies.subscribeTestimony();
+    const measureSubscription = Measures.subscribeMeasures();
+    const rdy = testimonySubscription.ready() && measureSubscription.ready();
+    const testimonyDoc = Testimonies.findOne({ _id: _id });
+    const measureData = Measures.find().fetch();
+    return {
+      measures: measureData,
+      testimony: testimonyDoc,
+      ready: rdy,
+    };
+  }, [_id]);
+
+  function getBillTitle(billNumber) {
+    const index = measures.map(function (measure) { return measure.measureNumber; }).indexOf(billNumber);
+    if (index !== -1) {
+      return measures[index].measureType.toUpperCase().concat(measures[index].measureNumber);
+    }
+    return '';
+  }
+
+  function getBillDescription(billNumber) {
+    const index = measures.map(function (measure) { return measure.measureNumber; }).indexOf(billNumber);
+    if (index !== -1) {
+      return measures[index].description;
+    }
+    return '';
+  }
+
+  return ready ? (
     <div>
-      <Container>
+      <Container id={PAGE_IDS.VIEW_TESTIMONY} className="view-testimony-container">
         <Row>
           <Col>
             <Button href={`/edit-testimony/${_id}`} variant="secondary" size="sm" className="bill-button-spacing">
@@ -24,10 +58,23 @@ const TestimonyPage = () => {
             </Button>
           </Col>
         </Row>
-        <Testimony ref={componentRef} />
+        <Testimony
+          ref={componentRef}
+          testimony={{ hearingDate: (testimony.hearingDate ? testimony.hearingDate.toDateString() : '-'),
+            hearingTime: (testimony.hearingDate ? testimony.hearingDate.toTimeString() : '-'),
+            hearingLocation: (testimony.hearingLocation ? testimony.hearingLocation : '-'),
+            committeeName: (testimony.committeeName ? testimony.committeeName : '-'),
+            department: '-',
+            testifier: (testimony.testifier ? testimony.testifier : ' '),
+            introduction: (testimony.introduction ? testimony.introduction : ''),
+            content: (testimony.content ? testimony.content : ''),
+            closing: (testimony.closing ? testimony.closing : ''),
+            measureTitle: (testimony.billNumber ? getBillTitle(testimony.billNumber) : ''),
+            measureDescription: (testimony.billNumber ? getBillDescription(testimony.billNumber) : '') }}
+        />
       </Container>
     </div>
-  );
+  ) : <LoadingSpinner message="Loading Testimony" />;
 };
 
 export default TestimonyPage;
