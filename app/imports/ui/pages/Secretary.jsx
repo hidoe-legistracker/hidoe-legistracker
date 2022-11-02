51e8ec7bde1892caf26d544598f419be69e7db8d
coleamparo <camparo@hawaii.edu>
2022-11-01 14:27:59 -1000

    Update Secretary.jsx

--- app/imports/ui/pages/Secretary.jsx
+++ app/imports/ui/pages/Secretary.jsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
 import { PAGE_IDS } from '../utilities/PageIDs';
+import { UserProfiles } from '../../api/user/UserProfileCollection';
+import { AdminProfiles } from '../../api/user/AdminProfileCollection';
 
-/* A simple static component to render some text for the landing page. */
-const Secretary = () => (
-  <Container id={PAGE_IDS.SECRETARY} className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
-      </Col>
+const Secretary = () => {
 
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
-      </Col>
+  const { user, ready } = useTracker(() => {
+    const currUser = Meteor.user() ? Meteor.user().username : '';
+    const userSubscription = UserProfiles.subscribe();
+    const adminSubscription = AdminProfiles.subscribe();
+    const rdy = userSubscription.ready() && adminSubscription.ready();
 
-    </Row>
-  </Container>
-);
+    let usr = UserProfiles.findOne({ email: currUser });
+    if (usr === undefined) {
+      usr = AdminProfiles.findOne({ email: currUser });
+    }
+
+    return {
+      user: usr,
+      ready: rdy,
+    };
+  });
+
+  // eslint-disable-next-line no-nested-ternary
+  return (ready ? (user.position === 'Office Secretary' ? (
+    <Container id={PAGE_IDS.SECRETARY} className="py-3">
+      <Row className="align-middle text-center">
+        <Col xs={4}>
+          <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+        </Col>
+
+        <Col xs={8} className="d-flex flex-column justify-content-center">
+          <h1>Welcome to this template</h1>
+          <p>Now get to work and modify this app!</p>
+        </Col>
+
+      </Row>
+    </Container>
+  ) : 'Must be a Secretary ') : <h1>Must be a Secretary</h1>);
+};
 
 export default Secretary;
